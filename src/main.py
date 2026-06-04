b1974a25010b7249fc23455f948258c018ec373d
YOONHO <Leave me alone>
2026-06-04 18:07:53 +0900

    submit

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 #
 # 강의 시간에 실습한 Linux 명령어를 활용하세요.
 def get_current_path_command():
-    raise NotImplementedError
+    return 'pwd'
 
 
 # 문제 2.
@@ -21,7 +21,10 @@ def get_current_path_command():
 #
 # 강의 시간에 사용한 옵션을 활용하세요.
 def make_list_command(hidden):
-    raise NotImplementedError
+    if hidden == True:
+        return 'ls -a'
+    else:return 'ls'
+    
 
 
 # 문제 3.
@@ -30,6 +33,7 @@ def make_list_command(hidden):
 #
 # path 인자는 이동할 디렉토리 경로입니다.
 def make_change_directory_command(path):
+    return 'cd '+path
     raise NotImplementedError
 
 
@@ -41,7 +45,7 @@ def make_change_directory_command(path):
 #
 # 강의 중 확인한 U-Boot 명령어를 활용하세요.
 def make_printenv_command(env_name):
-    raise NotImplementedError
+    return 'printenv '+env_name
 
 
 # 문제 5.
@@ -59,4 +63,4 @@ def make_printenv_command(env_name):
 #
 # 강의 중 실습한 bootargs 형식을 참고하세요.
 def make_bootargs_command(console, rootfs):
-    raise NotImplementedError
+    return f"setenv bootargs console={console} root={rootfs} rw"
